2fed5e6a53ac1151835aee913fdf4ccc0af254f4
umidkimsanboyev1 <umidmaster@yandex.com>
2021-12-22 16:46:36 +0500

    initial commit

--- src/main/java/max/value/App.java
+++ src/main/java/max/value/App.java
@@ -1,4 +1,16 @@
 package max.value;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class App {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+            api.registerBot(LibraryBot.getInstance());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/max/value/LibraryBot.java
+++ src/main/java/max/value/LibraryBot.java
@@ -1,4 +1,41 @@
 package max.value;
 
-public class LibraryBot {
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@NoArgsConstructor (access = AccessLevel.PRIVATE)
+public class LibraryBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        SendMessage msg = new SendMessage();
+        msg.setChatId(update.getMessage().getChatId().toString());
+        msg.setText("TEST");
+        try {
+            executeAsync(msg);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return "@mv_library_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5008623077:AAHhi81e-FvSxg0UNsaIA5Kak7EZnZOteZo";
+    }
+
+    private static LibraryBot instance = new LibraryBot();
+
+    public static LibraryBot getInstance() {
+        return instance;
+    }
 }
